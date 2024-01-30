bec958ac46d2d5b9cecc57dfca8886e543d43e96
Nikhilsharam962 <niksharma787683@gmail.com>
2024-01-30 12:17:37 +0530

    miniprojects

---
+++ project4/script.js
@@ -0,0 +1,28 @@
+/*let a = [1,2,3,4,5,6];
+for(let i= 0;i < a.length ;i++){
+console.log("i=",a[i]);
+}
+
+
+
+///splice method///
+
+let companies=["bloodery", "microsoft","uber","google","ibm","netflix"]
+companies.splice(2,1,"ola");
+console.log(companies)
+
+
+push methods///
+
+let companies=["bloodery", "microsoft","uber","google","ibm","netflix"]
+companies.push("amazon");
+console.log(companies);*/
+
+function myFunction(msg){
+    console.log(msg)
+}
+myFunction("i");
+
+
+
+
