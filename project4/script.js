a933def634eb58ec4441f6a717673bc04f10210f
Nikhilsharam962 <niksharma787683@gmail.com>
2024-03-06 22:53:11 +0530

    basics js

--- project4/script.js
+++ project4/script.js
@@ -1,31 +1,12 @@
-/*let a = [1,2,3,4,5,6];
-for(let i= 0;i < a.length ;i++){
-console.log("i=",a[i]);
-}
 
-
-
-///splice method///
-
-let companies=["bloodery", "microsoft","uber","google","ibm","netflix"]
-companies.splice(2,1,"ola");
-console.log(companies)
-
-
-push methods///
-
-let companies=["bloodery", "microsoft","uber","google","ibm","netflix"]
+let companies =["bloodery", "microsoft","uber","google","ibm","netflix"]
 companies.push("amazon");
 console.log(companies);
 
-functuion syntax */
+
 
 function myFunction(msg){
 console.log(msg);
 
 }
 myFunction( "I love js");
-
-
-
-
