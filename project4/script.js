20321a631bc8535644563df6a37e3bd750d607fc
Nikhilsharam962 <niksharma787683@gmail.com>
2024-03-14 23:06:28 +0530

    numbers

--- project4/script.js
+++ project4/script.js
@@ -1,78 +1,68 @@
 
+// /* Array syntax
+
+// let companies =["bloodery", "microsoft","uber","google","ibm","netflix"]
+// companies.push("amazon");
+// console.log(companies);
 
 
 
 
+//  fxn syntax example
+
+// function myFunction(msg){
+// console.log(msg);
+
+// }
+// myFunction( " love js");*/
+
+// //strings 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Array syntax
-
-let companies =["bloodery", "microsoft","uber","google","ibm","netflix"]
-companies.push("amazon");
-console.log(companies);
-
-
-
-
- fxn syntax example
-
-function myFunction(msg){
-console.log(msg);
-
-}
-myFunction( " love js");*/
-
-//strings 
-
-
-let str ="JavaScript"
-console.log(str)
+// let str ="JavaScript"
+// console.log(str)
  
-//class & object syntax
- class MyClass{
+// //class & object syntax
+//  class MyClass{
 
 
-    constructor(){}
+//     constructor(){}
 
 
-    myMethod(){}
+//     myMethod(){}
  
- }
+//  }
 
- let myobj =new MyClass()
+//  let myobj =new MyClass()
 
 
 
-//example class and object
+// //example class and object
  
-class ToyotaCar {
-    start() {
-        console.log("start");
-    }
+// class ToyotaCar {
+//     start() {
+//         console.log("start");
+//     }
 
-    stop(){
-        console.log("stop");
-    }
-}
+//     stop(){
+//         console.log("stop");
+//     }
+// }
 
-let fortuner = new ToyotaCar();
+// let fortuner = new ToyotaCar();
 
 
-//get elemmentBy id()
+//get elemmentBy id()//
+
+//Numbers//
+ 
+
+const x = 56.6559867645;
+x;
+const y = x.toFixed(2);
+y;
+
+
 
 
 
