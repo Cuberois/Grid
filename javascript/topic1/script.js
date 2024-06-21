2902fc71cbecc88112167abb41e44f98640831bd
Nikhilsharam962 <niksharma787683@gmail.com>
2024-06-21 13:02:43 +0530

    bulb on&off using DOM

--- javascript/topic1/script.js
+++ javascript/topic1/script.js
@@ -1,7 +1,27 @@
-var a = document.querySelector(".center")// seletion of an elemet----
-a.innerHTML  = "hello world" //changing HTML---
-a.style.color ="red" // changing Css alway write in camelcase---
-a.style.backgroundColor = "black"
-a.addEventListener("click",function(){
-    console.log("hello mitroo") // event listener---
-})
+// var a = document.querySelector(".center")// seletion of an elemet----
+// a.innerHTML  = "hello world" //changing HTML---
+// a.style.color ="red" // changing Css alway write in camelcase---
+// a.style.backgroundColor = "black"
+// a.addEventListener("click",function(){
+//     console.log("hello mitroo") // event listener---
+// })
+
+var a = document.querySelector(".center")
+var btn = document.querySelector("button")
+var flag = 0
+btn.addEventListener("click",function(){
+    if (flag == 0 ){
+    a.style.backgroundColor= "yellow"
+    console.log("lite aaegi")
+
+    flag=1 
+}else{
+    a.style.backgroundColor= "white"
+    console.log("lite chali gayi")
+
+    flag=0
+}
+
+}
+)
+
