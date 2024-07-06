68b962a16379ad9ab48c571c96fe4e4c563f8762
Nikhilsharam962 <niksharma787683@gmail.com>
2024-07-06 13:06:38 +0530

    basic typescript

---
+++ typescript/script.ts
@@ -0,0 +1,35 @@
+console.log("hello");               //syntax 
+
+
+// const num1: number = 10;
+// const num2: number = 20;
+// const res : number = num1+num2.
+// res();
+
+// console.log("hello"); 
+
+// let num = 8
+
+// num = "nikhil"
+
+ //////////////////////////////////// IF eLse statement
+if (true) 
+    {
+        console.log('This will always executed.');
+    }
+    
+    if (false) {
+        console.log('This will never executed.');
+    }
+
+
+    let x = 60, y = 20;
+
+    if (x > y) 
+    {
+        console.log('x is greater than y.');
+    } 
+    else
+    {
+        console.log('x is less than or equal to y.'); //This will be executed
+    }
