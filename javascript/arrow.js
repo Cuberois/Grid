42c2312945ec590bfa7d535e9198889ed91db52f
Nikhilsharam962 <niksharma787683@gmail.com>
2024-07-09 22:17:15 +0530

    basic arrow syntax

---
+++ javascript/arrow.js
@@ -0,0 +1,29 @@
+const abc = () => {
+    
+    return 12;                // syntax of arrow function
+}
+console.log(abc());
+
+//////////////////////////////////////////////////////////////////
+
+let myfxn =(a,b) => a*b        //multiple no.        
+console.log(myfxn(2,3));
+
+//////////////////////////////////////////////////////////
+  
+ abs =()=>{
+   console.log("hello")         //  syntax of arrow function
+}
+abs();
+
+hello = ()=> "world";
+  console.log(hello());
+
+
+
+  //////
+
+  
+(a,b)=> a+b;
+
+  
