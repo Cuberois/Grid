cd91d42ca9d8fb6497cd5306ca846d93e02b860f
Nikhilsharam962 <niksharma787683@gmail.com>
2024-07-10 13:46:37 +0530

    styling with tailwindcss

--- ts-project2/src/main.ts
+++ ts-project2/src/main.ts
@@ -1,17 +1,10 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
+
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
+  
+    <h1 className=" text-7xl text-center" >hello world</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
